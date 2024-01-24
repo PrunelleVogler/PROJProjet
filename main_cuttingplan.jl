70c0aa9ba6727cedcbd66a5bb7fda4e8e65b37cd
PrunelleVogler <prunelle-vogler@orange.fr>
2024-01-24 15:48:48 +0100

    Modif

--- main_cuttingplan.jl
+++ main_cuttingplan.jl
@@ -4,7 +4,7 @@ using Libdl
 
 
 function cutting_plane_algorithm()
-    include("Data/processed/20_USA-road-d.BAY.gr")
+    include("Data/processed/60_USA-road-d.BAY.gr")
     nb_cities = n
     nb_edges = size(Mat)[1]
     
@@ -60,12 +60,12 @@ function cutting_plane_algorithm()
             println("Solution master pb trouvee")
 
             # Modifier le pb 1 et le résoudre
-            new_objective1 = @expression(m1, sum((delta1[e] + Mat[e, 4]) * x_val[e] for e in 1:nb_edges))
+            new_objective1 = @expression(m1, sum((delta1[e] + Mat[e, 3]) * x_val[e] for e in 1:nb_edges))
             set_objective_function(m1, new_objective1)
             optimize!(m1)
 
             # Modifier le pb 2 et le résoudre
-            new_objective2 = @expression(m2, p[s] + p[t] + delta2[s] * ph[s] + delta2[t] * ph[t] + sum((delta2[i] * ph[i] + p[i]) * x_val[e] for i in 1:nb_cities if i !=s for e in 1:nb_edges if Mat[e][1] == i ))
+            new_objective2 = @expression(m2, p[s] + p[t] + delta2[s] * ph[s] + delta2[t] * ph[t] + sum((delta2[i] * ph[i] + p[i]) * y_val[i] for i in 1:nb_cities if i !=s for e in 1:nb_edges if Mat[e][1] == i ))
             set_objective_function(m2, new_objective2)
             optimize!(m2)
             
@@ -85,12 +85,12 @@ function cutting_plane_algorithm()
             end
             if z_val < obj1
                 delta1_val = JuMP.value.(delta1)
-                @constraint(m, z >= sum((delta1_val[e] + Mat[e, 4]) * x[e] for e in 1:nb_edges))
+                @constraint(m, z >= sum((delta1_val[e] + 1) * Mat[e, 3] * x[e] for e in 1:nb_edges))
                 println("Ajout coupe 1")
             end
             if obj2 > S
                 delta2_val = JuMP.value.(delta2)
-                @constraint(m, p[s] + p[t] + delta2_val[s] * ph[s] + delta2_val[t] * ph[t] + sum((delta2_val[i] * ph[i] + p[i]) * x[e] for i in 1:nb_cities if i !=s for e in 1:nb_edges if Mat[e][1] == i ) <= S)
+                @constraint(m, p[s] + p[t] + delta2_val[s] * ph[s] + delta2_val[t] * ph[t] + sum((delta2_val[i] * ph[i] + p[i]) * y[e] for i in 1:nb_cities if i !=s for e in 1:nb_edges if Mat[e][1] == i ) <= S)
                 println("Ajout coupe 2")
             end
         else
@@ -99,7 +99,8 @@ function cutting_plane_algorithm()
         iteration += 1
         #no_optimal_solution_found = false
     end
-
+    z_val = JuMP.value.(z)
+    println("Valeur objectif : ", z_val)
     # feasibleSolutionFound = primal_status(m) == MOI.FEASIBLE_POINT
     # isOptimal = termination_status(m) == MOI.OPTIMAL
 
