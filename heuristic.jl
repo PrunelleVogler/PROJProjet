49081ed536b301b5d0a90b5a126b3d96a32e2bf0
PrunelleVogler <prunelle-vogler@orange.fr>
2024-02-04 17:02:23 +0100

    Ajout heuristique

---
+++ heuristic.jl
@@ -0,0 +1,212 @@
+using JuMP
+using CPLEX
+using Libdl
+using TimerOutputs
+using Distributions
+epsilon = 0.0001
+instanceName = "Data/processed/20_USA-road-d.BAY.gr"
+
+function lecture(file)
+    if isfile(file)
+        # L’ouvrir
+        myFile = open(file)
+        # Lire toutes les lignes d’un fichier
+        data = readlines(myFile)
+        # Pour chaque ligne du fichier
+        i = 1
+        n, s, t, S, d1, d2 = 0, 0, 0, 0, 0, 0
+        p = zeros(10)
+        ph = zeros(10)
+        Mat = zeros(10, 4)
+        for line in data
+            # Afficher la ligne
+            if i == 1
+                tab = split(line, " ")
+                n = parse(Int64, tab[3])
+                p = zeros(n)
+                ph = zeros(n)
+                Mat = zeros(size(data)[1], 4)
+            end
+            if i == 2
+                tab = split(line, " ")
+                s = parse(Int64, tab[3])
+            end
+            if i == 3
+                tab = split(line, " ")
+                t = parse(Int64, tab[3])
+            end
+            if i == 4
+                tab = split(line, " ")
+                S = parse(Int64, tab[3])
+            end
+            if i == 5
+                tab = split(line, " ")
+                d1 = parse(Int64, tab[3])
+            end
+            if i == 6
+                tab = split(line, " ")
+                d2 = parse(Int64, tab[3])
+            end
+            if i == 7
+                tab1 = split(line, "p = [")
+                tab2 = split(tab1[2], "]")
+                tab = split(tab2[1], ", ")
+                j = 1
+                for element in tab
+                    p[j] = parse(Int64, element)
+                    j += 1
+                end
+            end
+            if i == 8
+                tab1 = split(line, "ph = [")
+                tab2 = split(tab1[2], "]")
+                tab = split(tab2[1], ", ")
+                j = 1
+                for element in tab
+                    ph[j] = parse(Int64, element)
+                    j += 1
+                end
+            end
+            if i > 9 && i < size(data)[1]
+                tab1 = split(line, ";")
+                tab = split(tab1[1], " ")
+                Mat[i - 9, 1] = parse(Int64, tab[1])
+                Mat[i - 9, 2] = parse(Int64, tab[2])
+                Mat[i - 9, 3] = parse(Int64, tab[3])
+                Mat[i - 9, 4] = parse(Float64, tab[4])
+            end
+            if i == size(data)[1]
+                tab1 = split(line, "]")
+                tab = split(tab1[1], " ")
+                Mat[i - 9, 1] = parse(Int64, tab[1])
+                Mat[i - 9, 2] = parse(Int64, tab[2])
+                Mat[i - 9, 3] = parse(Int64, tab[3])
+                Mat[i - 9, 4] = parse(Float64, tab[4])
+            end
+            i += 1
+        end
+        # Fermer le fichier
+        close(myFile)
+    end
+    return n, s, t, S, d1, d2, p, ph, Mat
+end
+
+function heuristic_algorithm(instanceName, timeLimit)
+    time_begin = time()
+    n, s, t, S, d1, d2, p, ph, Mat = lecture(instanceName)
+    nb_cities = n
+    nb_edges = size(Mat)[1]
+    nb_U1 = 10
+    nb_U2 = 10000
+    
+    #================Master problem==================#
+    
+    m = Model(CPLEX.Optimizer)
+    #set_optimizer_attribute(m, "CPX_PARAM_SCRIND", 0)
+    @variable(m, x[1:nb_edges], Bin)
+    @variable(m, y[1:nb_cities], Bin)
+    
+    # Contraintes de flot
+    @constraint(m, sum(x[e] for e in 1:nb_edges if convert(Int,Mat[e, 1]) == s) == 1)
+    @constraint(m, sum(x[e] for e in 1:nb_edges if convert(Int,Mat[e, 2]) == t) == 1)
+    @constraint(m, sum(x[e] for e in 1:nb_edges if convert(Int,Mat[e, 2]) == s) == 0)
+    @constraint(m, sum(x[e] for e in 1:nb_edges if convert(Int,Mat[e, 1]) == t) == 0)
+    for i in 1:nb_cities
+        if i !=s && i !=t
+            @constraint(m, sum(x[e] for e in 1:nb_edges if convert(Int,Mat[e, 1]) == i) == sum(x[e] for e in 1:nb_edges if convert(Int,Mat[e, 2]) == i))
+        end
+    end
+    #d_ij = Mat[ , 3]
+    #D_ij = Mat[ , 4]
+    # Contraintes U1 et U2
+
+    for i in 1:nb_U2
+        delta = zeros(nb_cities)
+        for k in 1:nb_cities
+            delta[k] = rand(Uniform(0, 2))
+        end
+        somme = sum(delta[k] for k in 1:nb_cities)
+        while somme > d2
+            k = round(rand(1:nb_cities))
+            delta[k] = 0
+            somme = sum(delta[k] for k in 1:nb_cities)
+        end
+        @constraint(m, sum(y[i] * (p[i] + delta[i] * ph[i]) for i in 1:nb_cities) <= S)
+    end
+    @objective(m, Min, 0)
+
+    x_val = zeros((nb_U1, nb_edges))
+    d_1 = zeros((nb_U1, nb_edges))
+    for k in 1:nb_U1
+        for e in 1:nb_edges
+            if 0 < Mat[e, 4]
+                d_1[k,e] = rand(Uniform(0, Mat[e, 4]))
+            end
+        end
+        somme = sum(d_1[k,e] for e in 1:nb_edges)
+        while somme > d1
+            e = rand(1:nb_edges)
+            d_1[k,e] = 0
+            somme = sum(d_1[k,e] for e in 1:nb_edges)
+        end
+        println(d_1[k])
+        new_objective = @expression(m, sum(x[e] * Mat[e, 3] * (1 + d_1[k,e]) for e in 1:nb_edges))
+        set_objective_function(m, new_objective)
+        optimize!(m)
+
+        x_val_prime = JuMP.value.(x)
+        for e in 1:nb_edges
+            x_val[k, e] = x_val_prime[e]
+        end
+        println("Modèle résolu")
+    end
+
+    Val_heuristic = 100000000
+    for k in 1:nb_U1
+        max_x_k = 0
+        for j in 1:nb_U1
+            d_1_x_k = sum(x_val[k, e] * Mat[e, 3] * (1 + d_1[k,e]) for e in 1:nb_edges)
+            if d_1_x_k > max_x_k
+                max_x_k = d_1_x_k
+            end
+        end
+        if max_x_k < Val_heuristic
+            Val_heuristic = max_x_k
+        end
+    end
+    println("val : ", Val_heuristic)
+    no_optimal_solution_found = true
+    iteration = 0
+    upper_bound = 0
+    lower_bound = 0
+    x_val = zeros(nb_edges)
+
+
+
+    # if time() - time_begin >= timeLimit
+    #     optimize!(m)
+    #     if primal_status(m) == MOI.FEASIBLE_POINT
+    #         upper_bound = 0
+    #         lower_bound = objective_value(m)
+    #     end
+    # end
+
+    # # feasibleSolutionFound = primal_status(m) == MOI.FEASIBLE_POINT
+    # # if feasibleSolutionFound
+    # #     println("Solution trouvée de ", s, " a ", t)
+    # #     vX = JuMP.value.(x)
+    # #     println("Valeurs de x")
+    # #     for e in 1:nb_edges
+    # #         if vX[e] == 1
+    # #             println(Mat[e, 1], " ", Mat[e, 2], " ")
+    # #         end
+    # #     end
+    # # end
+    # println("Upper bound ", upper_bound)
+    # println("Lower bound ", lower_bound)
+    # time_end = time()
+    # println("Time ", time_end - time_begin)
+    return upper_bound, lower_bound
+end
+
+heuristic_algorithm(instanceName, 2)
