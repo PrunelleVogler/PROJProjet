d4cdc35169233149206cd8fe562bd8b35b0d3e5b
ArchJeanne <jeanne.archambault@eleves.enpc.fr>
2024-02-04 23:16:02 +0100

    méthode branch and cut

--- main_main.jl
+++ main_main.jl
@@ -1,4 +1,4 @@
-time_limit = 30
+time_limit = 360
 
 ### Lecture de l'ensemble des instances
 # nb_villes = [20]
@@ -15,6 +15,29 @@ time_limit = 30
 #         push!(list_instances, "$(nb)_USA-road-d.$a.gr")
 #     end
 # end
+# list_instances = [
+#     "20_USA-road-d.NY.gr",
+#     "60_USA-road-d.COL.gr",
+#     "100_USA-road-d.BAY.gr",
+#     "140_USA-road-d.NY.gr",
+#     "180_USA-road-d.COL.gr",
+#     "250_USA-road-d.NY.gr",
+#     "350_USA-road-d.COL.gr",
+#     "450_USA-road-d.NY.gr",
+#     "550_USA-road-d.COL.gr",
+#     "650_USA-road-d.NY.gr",
+#     "750_USA-road-d.BAY.gr",
+#     "850_USA-road-d.COL.gr",
+#     "950_USA-road-d.NY.gr",
+#     "1100_USA-road-d.NY.gr",
+#     "1300_USA-road-d.NY.gr",
+#     "1500_USA-road-d.NY.gr",
+#     "1700_USA-road-d.NY.gr",
+#     "1900_USA-road-d.BAY.gr",
+#     "2100_USA-road-d.COL.gr",
+#     "2300_USA-road-d.BAY.gr",
+#     "2500_USA-road-d.NY.gr"
+# ]
 list_instances = [
     "20_USA-road-d.NY.gr",
     "60_USA-road-d.COL.gr",
@@ -24,34 +47,21 @@ list_instances = [
     "250_USA-road-d.NY.gr",
     "350_USA-road-d.COL.gr",
     "450_USA-road-d.NY.gr",
-    "550_USA-road-d.COL.gr",
-    "650_USA-road-d.NY.gr",
-    "750_USA-road-d.BAY.gr",
-    "850_USA-road-d.COL.gr",
-    "950_USA-road-d.NY.gr",
-    "1100_USA-road-d.NY.gr",
-    "1300_USA-road-d.NY.gr",
-    "1500_USA-road-d.NY.gr",
-    "1700_USA-road-d.NY.gr",
-    "1900_USA-road-d.BAY.gr",
-    "2100_USA-road-d.COL.gr",
-    "2300_USA-road-d.BAY.gr",
-    "2500_USA-road-d.NY.gr"
+    "550_USA-road-d.COL.gr"
 ]
-
 ### Ouvrir fichier txt
 
 
 ### Executer dual/cutting plane/ branch and cutting et remplir le fichier txt
 include("main_cuttingplan.jl")
 # file_name = "cutting_plane"*"-"*string(minimum(nb_villes))*"-"*string(maximum(nb_villes))*"-"*string(time_limit)*"s.txt"
+# file_name = "cutting_plane-comparaison-méthodes"*string(time_limit)*"s.txt"
 
 # include("main_dual.jl")
 # file_name = "dual"*"-"*string(minimum(nb_villes))*"-"*string(maximum(nb_villes))*"-"*string(time_limit)*"s.txt"
 
 # include("branch_and_cut.jl")
 # file_name = "branch_and_cut"*"-"*string(minimum(nb_villes))*"-"*string(maximum(nb_villes))*"-"*string(time_limit)*"s.txt"
-file_name = "cutting_plane-comparaison-méthodes"*string(time_limit)*"s.txt"
 file = open(file_name,"w")
 
 for instance in list_instances
